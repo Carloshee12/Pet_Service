e0e9c015f5afac4de5c741da3c865dcee24354c0
Athos Reis <athos3655@gmail.com>
2025-02-19 21:36:42 -0300

    criando regras de validação do agendamento

--- js/agendamento_validation.js
+++ js/agendamento_validation.js
@@ -1,7 +1,7 @@
 // Validação Nome
-let nomeInput = document.getElementById("name")
-let nomeLabel = document.querySelector('label[for="name"]')
-let nomeHelper = document.getElementById("name-helper")
+let nomeInput = document.getElementById("name");
+let nomeLabel = document.querySelector('label[for="name"]');
+let nomeHelper = document.getElementById("name-helper");
 
 nomeInput.addEventListener("focus", () => {
     nomeLabel.classList.add("required-popup")
@@ -22,6 +22,67 @@ nomeInput.addEventListener("change", (e) => {
         nomeInput.classList.add("correct")
         nomeInput.classList.remove("error")
         nomeHelper.classList.remove("visible")
+
+    }
+})
+//----------------------------------------------------
+
+//-------------------------------------------------------------------------------
+
+// // Validação email
+let emailInput = document.getElementById("email")
+let emailLabel = document.querySelector('label[for="email"]')
+let emailHelper = document.getElementById("email-helper")
+
+emailInput.addEventListener("focus", () => {
+    emailLabel.classList.add("required-popup")
+})
+
+emailInput.addEventListener("blur", () => {
+    emailLabel.classList.remove("required-popup")
+})
+
+
+emailInput.addEventListener("change", (e) => { 
+    if(e.target.value.includes("@") && e.target.value.includes(".com")){
+        emailInput.classList.add("correct")
+        emailInput.classList.remove("error")
+        emailHelper.classList.remove("visible")
+
+    }else{
+        emailInput.classList.add("error")
+        emailInput.classList.remove("correct")
+        emailHelper.classList.add("visible")
+
+    }
+})
+
+//-------------------------------------------------------------------------------
+
+// // Validação nascimento
+let nascInput = document.getElementById("birthdate")
+let nascLabel = document.querySelector('label[for="birthdate"]')
+let nascHelper = document.getElementById("birthdate-helper")
+
+nascInput.addEventListener("focus", () => {
+    nascLabel.classList.add("required-popup")
+})
+
+nascInput.addEventListener("blur", () => {
+    nascLabel.classList.remove("required-popup")
+})
+
+
+nascInput.addEventListener("change", (e) => { 
+    if((new Date().getFullYear() - new Date(nascInput.value).getFullYear()) >= 20){
+        nascInput.classList.add("error")
+        nascInput.classList.remove("correct")
+        nascHelper.classList.add("visible")
+
+    }else{
+        nascInput.classList.remove("error")
+        nascInput.classList.add("correct")
+        nascHelper.classList.remove("visible")
 
     }
 })
